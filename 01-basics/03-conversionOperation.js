9a3a0888a4a84ed7df9958673c5c9d71239630fd
Rakhi Bhagwat <93554809+Rakhi-2002@users.noreply.github.com>
2025-05-04 18:29:02 +0000

    Understanding Operations in JS

--- 01-basics/03-conversionOperation.js
+++ 01-basics/03-conversionOperation.js
@@ -1,12 +1,12 @@
 let score = true
 
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
 //converting string score into number
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 
 /*
@@ -22,8 +22,8 @@ Notes: Conversion of various datatypes into a Number and their output
 let isLoggedIn = "Rakhi"
 let boolIsLoggedIn = Boolean(isLoggedIn)
 
-console.log(typeof isLoggedIn);
-console.log(boolIsLoggedIn);
+// console.log(typeof isLoggedIn);
+// console.log(boolIsLoggedIn);
 
 /*
 Notes: Conversion of various data types into Boolean
@@ -35,13 +35,48 @@ Notes: Conversion of various data types into Boolean
 
 let num = 1234
 let stringNum = String(num)
-console.log(typeof num);
-console.log(typeof stringNum);
+// console.log(typeof num);
+// console.log(typeof stringNum);
 
 /*
 Notes: Conversion of various data types into String
 - 1234 = string -- any number converts into string
 */
+
+
+//******************************** Operations in JS ************************** */
+
+let value = 3
+let negOfVal = -value
+console.log(negOfVal);
+console.log(34+8);
+console.log(2^10);
+console.log(2**5);
+console.log(5/2);
+
+let str1 = "Rakhi"
+let str2 = " Bhagwat"
+console.log(str1 + str2);
+
+console.log("12" + 6);
+console.log("1" + 2 + 5);
+console.log(1 + 3 + "2");       //Not a good practice in corperate
+console.log(true);
+
+//All below kinds of code is not recommended, not a good practice. Reflects poor readability
+console.log(+true);
+console.log(+"");
+
+
+
+
+
+
+
+
+
+
+
 
 
 

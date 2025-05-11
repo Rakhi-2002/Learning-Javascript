3ba995beea54752922858ac5963d7fbfcdcc3f90
Rakhi Bhagwat <93554809+Rakhi-2002@users.noreply.github.com>
2025-05-11 16:47:51 +0000

    Understanding Stack(call by value) and Heap(call by ref) storage

--- 01-basics/Datatypes-Summary.js
+++ 01-basics/Datatypes-Summary.js
@@ -1,3 +1,5 @@
+ //----------------------- Summary on Imp data types in JS ----------------------------------------------
+ 
  const score = 100;
  const scoreVal = 100.3;
  const isLoggedIn = true;
@@ -25,4 +27,47 @@
 
  console.table([typeof heros, typeof obj, typeof tempFunc]);
  
+ //---------------- Understanding Stack and Heap usage in data storage ----------------
+
+ //Stack: Call by Value Example
+ let name = "Rakhi";
+ let anotherName = name;
+
+ anotherName = "Bhagwat";
  
+ console.log(name);
+ console.log(anotherName);
+
+ //Heap: Call by Reference Example 
+ let userInfo = {
+   name: "Rutika",
+   upiId: "rutika23@ybl"
+ }
+ console.log(userInfo);
+
+ let AnotherUser = userInfo;
+ AnotherUser.name = "Priya";
+
+ console.log(AnotherUser);
+ console.log(userInfo);
+
+ function print(){
+   //Stored in Stack
+   let name = "Anjali";
+   let salary = 30000;
+
+   console.table([name, salary]);
+
+   //Stored in Heap
+   let roleDesc = {
+      role: "Java Developer",
+      location: "Pune",
+      empId: 123456,
+      name: name,    //pass by value is used, so copy of name will be passed and not its reference
+      name: "Anita"
+   }
+   console.log(roleDesc);
+   console.log(name);
+ }
+
+ print();

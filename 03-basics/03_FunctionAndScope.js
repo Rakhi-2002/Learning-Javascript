890d35fb2f0a4875824816cd6bae9a14cfec551f
Rakhi Bhagwat <93554809+Rakhi-2002@users.noreply.github.com>
2025-10-26 15:43:08 +0000

    understanding scope with functions

--- 03-basics/03_FunctionAndScope.js
+++ 03-basics/03_FunctionAndScope.js
@@ -15,3 +15,45 @@ if(true){
 // console.log(b);      //gives error as we don't have globally declared b
 console.log(c);
 
+//child function (two()) can access things from parent function (one())
+function one(){
+  const username = "Rakhi"
+
+  function two(){
+    const website = "youtube"
+    console.log(username);
+  }
+  // console.log(website);    //it gives error as website is not defined anywhere in func one()
+  
+  // two();   //this func have scope or can access outside variables as well as its own function scope (two())
+
+}
+
+one();
+
+if(true){
+  const username = "rakhi"
+  if(username === "rakhi") {
+    const website = "Youtube"
+    console.log(username + " " + website);
+  }
+  // console.log(website);    //gives error as website is not in this scope
+  
+}
+// console.log(username);    //gives error as username is not in global scope, its declared inside if block, so its scope is limited to it
+
+addOne(7)   //No error, its accessible as it behaves as a function
+
+function addOne(num){
+  return num + 1;
+}
+
+// addTwo(61)     //gives Error: Cannot access 'addTwo' before initialization (Hoisting concept)
+
+const addTwo = function(num){
+  return num + 2
+}
+
+// console.log(addOne());
+
+
